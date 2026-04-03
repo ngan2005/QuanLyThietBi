aa18008fe745c5b89b870e3ff1769a216324eb8e
ngan2005@ <luu.kimngan205@gmail.com>
2026-04-04 01:12:34 +0700

    Khởi tạo dự án Spring Boot Quản lý thiết bị

---
+++ src/main/java/com/example/QuanLyThietBi/QuanLyThietBiApplication.java
@@ -0,0 +1,13 @@
+package com.example.QuanLyThietBi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLyThietBiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanLyThietBiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/QuanLyThietBi/QuanLyThietBiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QuanLyThietBi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanLyThietBiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
